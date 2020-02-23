b68e5f7e73ae644ad2a30c4a23ed7e1d98fbeee3
welsemil <welsemil@fel.cvut.cz>
2020-02-23 20:29:21 +0100

    Version 2.0
    Version 2.0 of Lifesim has 2 modes, where either the last generation is
    used for evaluation or only one table is used at the same time.
    It also features 5 presets.

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
-## Lifesim v1.0
-
-Lifesim is a simple shell based life simulation in a 2D grid (10x10 by default).
+## Lifesim v2.0
+Lifesim v2.0 is a simple shell based life simulation in a 2D grid with multiple
+preset initial states
 
 There are only 2 states - alive or dead.
+
+
+
+The current version has 5 presets, 3 of which are oscilating with period of 2,
+one dies out eventually and the remaining is a moving wing.
--- lifesim.py
+++ lifesim.py
@@ -1,33 +1,122 @@
-from life import Life
+#### Lifesim v2.0
+import time
+import copy
+import os
 
-W = 10
+W = None
 
-grid = []
+# blinker
+blinker = [ [0,0,0,0,0],
+            [0,0,0,0,0],
+            [0,1,1,1,0],
+            [0,0,0,0,0],
+            [0,0,0,0,0] ]
 
-if grid == []:
-    for i in range(W):
-        grid.append([])
+blinkers = [ [0,0,0,0,0,0],
+            [0,1,1,1,0,0],
+            [0,0,0,0,0,0],
+            [0,0,0,0,0,0],
+            [0,0,1,1,1,0],
+            [0,0,0,0,0,0] ]
 
-    for y in range(W):
-        for x in range(W):
-            grid[y].append(Life(grid, x, y, random=False, state=1 if x==2 or y==2 else 0))
+toad = [[0,0,0,0,0,0],
+        [0,0,0,0,0,0],
+        [0,0,1,1,1,0],
+        [0,1,1,1,0,0],
+        [0,0,0,0,0,0],
+        [0,0,0,0,0,0],
+]
 
+glider = [[0,1,0,0,0,0,0],
+          [0,0,1,0,0,0,0],
+          [1,1,1,0,0,0,0],
+          [0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0]
+]
 
+beacon = [[0,0,0,0,0,0],
+          [0,1,1,0,0,0],
+          [0,1,1,0,0,0],
+          [0,0,0,1,1,0],
+          [0,0,0,1,1,0],
+          [0,0,0,0,0,0],
+]
+
+presets = [blinker, blinkers, toad, glider, beacon]
 
 def printGrid(grid):
-    print("=====================")
+    delimiter = ["=" for i in range(2*W + 1)]
+    delimiter = ''.join(delimiter)
+    print(delimiter) 
     for y in range(W):
         print("|", end='')
         for x in range(W-1):
-            print(grid[y][x], end=' ')
-        print(grid[y][W-1], end='|\n')
-    print("=====================")
+            print("■" if grid[y][x] == 1 else " ", end=' ')
+        print("■" if grid[y][W-1] == 1 else " ", end='|\n')
+    print(delimiter)
 
 
+def getNBids(grid, x, y):
+    w = len(grid[0])
+    h = len(grid)
+    temp = [(x-1, y-1),
+        (x,y-1), 
+        (x+1,y-1), 
+        (x-1,y), 
+        (x+1,y), 
+        (x-1,y+1), 
+        (x,y+1), 
+        (x+1,y+1), 
+            ]
+    nbs = []
+    for x,y in temp:
+        if(x >= 0 and x < w):
+            if(y >= 0 and y < h):
+                nbs.append((x,y))
+    return nbs
+
+def countLivingNBS(grid, pointx, pointy):
+    ids = getNBids(grid, pointx, pointy)
+    total = 0
+    for x, y in ids:
+        total += grid[y][x]
+    return total
 
 
-if __name__ == '__main__':
-    print("LifeSim v1.0")
-    #input('Press enter to start')
+def nextGen(grid, backup=True):
+    if(backup):
+        ret = copy.deepcopy(grid)
+    else:
+        ret = grid
+    for y in range(W):
+        for x in range(W):
+            count = countLivingNBS(grid, x, y)
+            if(grid[y][x] == 1 and (count < 2 or count > 3)):
+                ret[y][x] = 0
+            elif(grid[y][x] == 0 and count == 3):
+                ret[y][x] = 1
+            else:
+                ret[y][x] = grid[y][x]
+    return ret
+
+clear = lambda: os.system("cls")
+
+if __name__ == "__main__":
+    backup = True
+    clear()
+    print("Lifesim v2.0")
+    msg = "Choose a preset - blinker(0), blinkers(1), toad(2), glider(3),\nbeacon(4): "
+    grid = presets[int(input(msg))]
+    W = len(grid)
     printGrid(grid)
-    #input('Press enter to end')
+    iters = int(input("How many iterations: "))
+    time.sleep(0.25)
+    print("OK")
+    time.sleep(0.25)
+    for i in range(iters+1):
+        clear()
+        print("Generation", i)
+        printGrid(grid)
+        grid = nextGen(grid, backup)
+        time.sleep(1)
